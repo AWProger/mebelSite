459e7d1c48d5fadfd091a622311cb6cbc4f2a38c
AWProger <denggy@yandex.ru>
2026-01-16 01:53:27 +0300

    отправка заявки на почту

---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+if($_SERVER['REQUEST_METHOD']!=='POST')exit;
+$name=trim($_POST['name']??'');
+$phone=trim($_POST['phone']??'');
+if(!$name||!$phone)exit;
+$data=date('Y-m-d H:i:s')." | $name | $phone\n";
+file_put_contents('unik.txt',$data,FILE_APPEND);
+mail('denggy@yandex.ru','Новая заявка',"Имя: $name\nТелефон: $phone","Content-Type: text/plain; charset=UTF-8");
+echo 'OK';
